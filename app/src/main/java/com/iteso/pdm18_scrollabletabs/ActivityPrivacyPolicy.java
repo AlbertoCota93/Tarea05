52f19bf1944d6fbe8001f29d8745bc615c7683cc
AlbertoCota93 <is694734@iteso.mx>
2018-10-05 14:20:10 -0500

    WebView works, work complete

--- app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
+++ app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
@@ -1,11 +1,14 @@
 package com.iteso.pdm18_scrollabletabs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class ActivityPrivacyPolicy extends AppCompatActivity {
@@ -21,6 +24,7 @@ public class ActivityPrivacyPolicy extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebAppInterface(this),"Android");
+        webView.setWebViewClient(new MyWebClient());
 
     }
     public class WebAppInterface{
@@ -31,6 +35,17 @@ public class ActivityPrivacyPolicy extends AppCompatActivity {
         @JavascriptInterface
             public void showToast(String toast){
             Toast.makeText(mContext,toast, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class MyWebClient extends WebViewClient{
+        public boolean shouldOverrideUrlLoading (WebView view, String url){
+            if(Uri.parse(url).getHost().equals("www.iteso.mx")){
+              return false;
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+            startActivity(intent);
+            return true;
         }
     }
 }
